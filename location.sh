8ceba9ba63c7bca4417efa1e000455c58efffac2
Xployt <43792836+Xpl0yt91@users.noreply.github.com>
2021-07-10 16:11:46 +0100

    Tidy Up

--- location.sh
+++ location.sh
@@ -11,7 +11,7 @@ tee <<-EOF
 ↘️  SETTING: Backup Download Location
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 Paying attention to your disk space is important! Type the name of your
-location is very important! Keep in mind PG does not format the
+location is very important! Keep in mind Pandaura does not format the
 location for you!
 
 Examples:
@@ -42,29 +42,29 @@ if [ "$typed" == "" ]; then
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-  sleep 3
-  bash /opt/plexguide/menu/interface/serverid.sh
-  exit
+    sleep 3
+    bash /opt/plexguide/menu/interface/serverid.sh
+    exit
 else
-
-  ##################################################### TYPED CHECKERS - START
-  typed2=$typed
-  bonehead=no
-  ##### If BONEHEAD forgot to add a / in the beginning, we fix for them
-  initial="$(echo $typed | head -c 1)"
-  if [ "$initial" != "/" ]; then
-    typed="/$typed"
-    bonehead=yes
-  fi
-  ##### If BONEHEAD added a / at the end, we fix for them
-  initial="${typed: -1}"
-  if [ "$initial" == "/" ]; then
-    typed=${typed::-1}
-    bonehead=yes
-  fi
-
-  ##### Notify User is a Bonehead
-  if [ "$bonehead" == "yes" ]; then
+    
+    ##################################################### TYPED CHECKERS - START
+    typed2=$typed
+    bonehead=no
+    ##### If BONEHEAD forgot to add a / in the beginning, we fix for them
+    initial="$(echo $typed | head -c 1)"
+    if [ "$initial" != "/" ]; then
+        typed="/$typed"
+        bonehead=yes
+    fi
+    ##### If BONEHEAD added a / at the end, we fix for them
+    initial="${typed: -1}"
+    if [ "$initial" == "/" ]; then
+        typed=${typed::-1}
+        bonehead=yes
+    fi
+    
+    ##### Notify User is a Bonehead
+    if [ "$bonehead" == "yes" ]; then
     tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -75,16 +75,16 @@ You Typed : $typed2
 Changed To: $typed
 
 EOF
-    read -n 1 -s -r -p "Press [ANY KEY] to Continue "
-    echo ""
-  fi
-
-  #################################################### TYPED CHECKERS - END
-
-  mkdir "$typed/pgcheck" &>/dev/null
-  # Recalls for to check existance
-  rcheck=$(ls -la $typed | grep "\<pgcheck\>")
-  if [ "$rcheck" == "" ]; then
+        read -n 1 -s -r -p "Press [ANY KEY] to Continue "
+        echo ""
+    fi
+    
+    #################################################### TYPED CHECKERS - END
+    
+    mkdir "$typed/pgcheck" &>/dev/null
+    # Recalls for to check existance
+    rcheck=$(ls -la $typed | grep "\<pgcheck\>")
+    if [ "$rcheck" == "" ]; then
     tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -92,10 +92,10 @@ EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-    sleep 4
-    exit
-  fi
-
+        sleep 4
+        exit
+    fi
+    
   tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -103,10 +103,10 @@ EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-  # Prevents From Repeating
-  rm -rf $typed/pgcheck
-  sleep 3
-
+    # Prevents From Repeating
+    rm -rf $typed/pgcheck
+    sleep 3
+    
   tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -114,7 +114,7 @@ EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 EOF
-  echo "$typed" >/var/plexguide/data.location
-  read -n 1 -s -r -p "Press [ANY] Key to Continue "
-
+    echo "$typed" >/var/plexguide/data.location
+    read -n 1 -s -r -p "Press [ANY] Key to Continue "
+    
 fi
